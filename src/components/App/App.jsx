0d86a1173629c144c5e4931d29bd4b3206a0b5d6
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-30 11:02:00 -0500

    started up the installs as well as started the HTML piece in app.jsx for our portion.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+
+
+
+
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+      <h2>Step 1: Select your Pizza</h2>
+      <div>
+        <ul>
+          <li>
+            
+
+            
+          </li>
+        </ul>
+
+      </div>
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './components/App/App';
+
+//This is our Redux store.
+const storeInstance = createStore(
+    combineReducers({
+
+
+    }),
+    applyMiddleware(logger)
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store={storeInstance} >
         <App />
+        </Provider>
     </React.StrictMode>
 );
