9756cea3c6b0e239296ae12d0818162b1dff9230
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-30 13:57:15 -0500

    testing for fue

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import axios from 'axios';
 
 import './App.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import {useEffect} from 'react';
 
 function App() {
+
+
+
     const dispatch = useDispatch();
+
+    const cart = useSelector(store => store.cart)
+
     const fetchPizza = ()=> {
 console.log('Fetching pizzas!');
 
@@ -23,26 +31,43 @@ console.log('Fetching pizzas!');
   
     }
 
+    useEffect(()=>{
+        fetchPizza();
+    })
+
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
+      <Router>
+      <Route exact path='/'>
       <h2>Step 1: Select your Pizza</h2>
+     
       <div>
         <ul>
-          <li>
-            
-
-            
-          </li>
+          {cart.map(pizza =>{
+            return (
+              <li key={pizza.id}>
+                <img src={pizza.image_path}></img>
+                <h3>{pizza.name}</h3>
+                <p>{pizza.description}</p>
+                <p>{pizza.price}</p>
+                <button>Add</button>
+              </li>
+            )
+          })}
+        
         </ul>
 
       </div>
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+  </Route>
+  </Router>
     </div>
+   
   );
 }
 
